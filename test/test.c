1a1f1f56dc16cec3a88b25acae7d6d05b7984602
da <>
2024-06-24 14:18:27 +0200

    add experimental texture animation

--- test/test.c
+++ test/test.c
@@ -8,16 +8,21 @@
 /* usage */
 __attribute__((visibility("default"))) int generate_textures(state_t* state, float dt)
 {
+    static float timer = 0; timer += dt; // if (timer > 1.0f) { timer = 0; }
+
+    float zero_to_one = (sinf(timer) + 1)/2;
+
     color_t RED    = {1,0,0,1};
     color_t GREEN  = {0,1,0,1};
     color_t BLUE   = {0,0,1,1};
 
-    color_t VIOLET = {1,0,1,1};
+    color_t VIOLET = {0.8,0,0.4,1};
+
 
     /* NOTE: for opengl, end with flip() operation to match origin at top-left */
     texture_t tex  = tex_build(texture(32,32,GREEN),
-                               noise(1.2), circle(16,26,8,RED), rect(5,18,4,4, RED),
-                               rect(0,14,32,4,RED),
+                               noise(zero_to_one + 0.8), circle(16,16,(int)(zero_to_one*8),RED),
+                               rect(0,(int) (zero_to_one * 29),32,4, VIOLET), rect(0,32,32,4,RED),
                                flip()
                               );
 
