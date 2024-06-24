b2eb2858dfe7edccb4af540a3a470a64c28f64a6
da <>
2024-06-24 20:10:14 +0200

    add macros for a new style of api

--- test/test.c
+++ test/test.c
@@ -41,21 +41,62 @@ __attribute__((visibility("default"))) int generate_textures(state_t* state, flo
     /* texture atlas usage code */
     {
         color_t NONE    = {0,0,0,0};
-        texture_t atlas = tex_build(texture(96,96, NONE));
-        texture_t tex_1 = tex_build(subtexture(&atlas,32,32,RED), noise(zero_to_one + 0.1));
-        texture_t tex_2 = tex_build(subtexture(&atlas,32,32,BLUE));
-        texture_t tex_3 = tex_build(subtexture(&atlas,32,32,GREEN));
-        texture_t tex_4 = tex_build(subtexture(&atlas,32,32,VIOLET));
-        texture_t tex_9 = tex_build(subtexture(&atlas,32,32,GREEN),
-                                   noise(zero_to_one + 0.8), circle(16,16,(int)(zero_to_one*8),RED),
-                                   rect(0,(int) (zero_to_one * 29),32,4, VIOLET), rect(0,32,32,4,RED),
-                                   flip()
-                                  );
-        texture_t tex_5 = tex_build(subtexture(&atlas,32,32,YELLOW));
-        texture_t tex_6 = tex_build(subtexture(&atlas,32,32,BROWN));
-        texture_t tex_7 = tex_build(subtexture(&atlas,32,32,ANIMATED_COLOR));
-        texture_t tex_8 = tex_build(subtexture(&atlas,32,32,CYAN));
-        //texture_t tex_9 = tex_build(subtexture(&atlas,32,32,PURPLE));
+        //texture_t atlas = tex_build(texture(96,96, NONE));
+        //texture_t tex_1 = tex_build(_subtexture(&atlas,32,32,RED), noise(zero_to_one + 0.1));
+        //texture_t tex_2 = tex_build(_subtexture(&atlas,32,32,BLUE));
+        //texture_t tex_3 = tex_build(_subtexture(&atlas,32,32,GREEN));
+        //texture_t tex_4 = tex_build(_subtexture(&atlas,32,32,VIOLET));
+        //texture_t tex_X = tex_build(_subtexture(&atlas,32,32,GREEN),
+        //                           noise(zero_to_one + 0.8), circle(16,16,(int)(zero_to_one*8),RED),
+        //                           rect(0,(int) (zero_to_one * 29),32,4, VIOLET), rect(0,32,32,4,RED),
+        //                           flip()
+        //                          );
+        //texture_t tex_5 = tex_build(_subtexture(&atlas,32,32,YELLOW));
+        //texture_t tex_6 = tex_build(_subtexture(&atlas,32,32,BROWN));
+        //texture_t tex_7 = tex_build(_subtexture(&atlas,32,32,ANIMATED_COLOR));
+        //texture_t tex_8 = tex_build(_subtexture(&atlas,32,32,CYAN));
+
+        //texture_t tex_9 = tex_build(_subtexture(&atlas,32,32,PURPLE));
+
+        // scope_tex_build(atlas, texture(96,96))
+        // {
+        //     color(RED)   // _color(temp, ...);
+        //     noise(1)     // _noise(temp, ...);
+        //     scope_tex_rect() // temp.         temp.x_pos = x_pos;
+        //         size_t x_start, y_start; size_t width, height;
+        //     for (size_t old_x_start = temp.x_start, old_y_start = temp.y_start, ; temp.j == 0; (temp.i += 1, atlas = _create(atlas)))
+        //     {
+        //
+        //     }
+        // }
+
+        #define lerp(t, a, b) (a + t * (b - a))
+
+        /* new api */
+        texture_t atlas;
+        scope_tex_build(atlas, texture(96,96, GREEN)) {
+            scope_tex_rect(44,8,64,16) {
+                color(RED);
+                scope_tex_rect(4,lerp(zero_to_one,4,44),16,8) {
+                    color(BLUE);
+                    noise(0.8);
+                    pixel();
+                }
+            }
+            flip();
+        }
+
+        /* stress test */
+        //texture_t atlas = tex_build(texture(96,96, GREEN));
+        //{
+        //    for (int i = 0; i < 7; i++)
+        //    {
+        //        tex_build(_subtexture(&atlas,32,32,ANIMATED_COLOR),
+        //                  noise(zero_to_one + 0.2)
+        //                  //, circle(16,16,(int)(zero_to_one*8),RED)
+        //        );
+        //    }
+        //}
 
         state->tex[0] = atlas;
     }
--- tex_builder.h
+++ tex_builder.h
@@ -55,7 +55,19 @@ typedef struct texture_t
 } texture_t;
 
 /* wrapper for type safety */
-typedef struct tex_builder_t { texture_t tex;                             } tex_builder_t;
+typedef struct tex_builder_t {
+    texture_t tex;
+
+    size_t atlas_width;
+    size_t atlas_height;
+
+    /* bounds of where we are currently drawing */
+    int x_start, y_start;
+    int width, height;
+
+    /* used in for loop macros */
+    int i;
+} tex_builder_t;
 
 /*
  * api
@@ -63,24 +75,80 @@ typedef struct tex_builder_t { texture_t tex;                             } tex_
 /* building api (allocating) */
 tex_builder_t texture(int w, int h, color_t rgb);                     // use for creating texture atlas
 tex_builder_t tex_copy(texture_t tex);                                // deep copy a texture
+
 /* building api (non-allocating) */
-tex_builder_t subtexture(texture_t* atlas,int w, int h, color_t rgba); // get a texture from an atlas
+
+#define subtexture(...) _subtexture(&temp.tex, __VA_ARGS__)
+tex_builder_t _subtexture(texture_t* atlas,int w, int h, color_t rgba); // get a texture from an atlas
 
 /* transformation api */
-#define noise(...) _noise(&temp, __VA_ARGS__)
+//#define noise(...) _noise(&temp, __VA_ARGS__) // TODO old
+#define noise(...) __noise(temp, __VA_ARGS__)
 void   _noise(tex_builder_t* tex, float intensity);
+void  __noise(tex_builder_t  tex, float intensity);
 #define grunge(...) _grunge(&temp, __VA_ARGS__)
 void   _grunge(tex_builder_t* tex, float intensity); /* TODO */
 #define smear(...) _smear(&temp, ##__VA_ARGS__)
 void   _smear(tex_builder_t* tex, color_t rgb);      /* TODO */
 #define rect(...) _rect(&temp, __VA_ARGS__)
 void   _rect(tex_builder_t* tex, unsigned int x, unsigned int y, unsigned int width, unsigned int height, color_t color);
+
+static tex_builder_t   __rect(tex_builder_t texer, unsigned int x, unsigned int y, unsigned int width, unsigned int height)
+{
+    tex_builder_t temp = texer;
+    temp.x_start = x;
+    temp.y_start = y;
+    temp.width   = width;
+    temp.height  = height;
+    return temp;
+}
+
+#define color(...) _color(temp, __VA_ARGS__)
+static void   _color(tex_builder_t tex, color_t color)
+{
+    /* TODO check if we are out of bounds */
+
+    /* color the subtexture */
+    for (size_t y = tex.y_start; y < (tex.y_start + tex.height); y++) {
+        for (size_t x = tex.x_start; x < (tex.x_start + tex.width); x++) {
+            size_t index = (y * tex.atlas_width) + x;
+//            if (x < tex.atlas_width && y < tex.atlas_height) {
+                tex.tex.rgb[index] = color;
+//            }
+        }
+    }
+}
+
+/* for debugging */
+#define pixel(...) _pixel(temp, ##__VA_ARGS__)
+static void   _pixel(tex_builder_t tex)
+{
+   size_t index = (tex.y_start * tex.atlas_width) + tex.x_start;
+   tex.tex.rgb[index] = (color_t){1,1,1,1};
+}
+
 #define circle(...) _circle(&temp, __VA_ARGS__)
 void   _circle(tex_builder_t* tex, unsigned int x, unsigned int y, unsigned int radius, color_t color);
 #define flip(...) _flip(&temp, ##__VA_ARGS__)
 void   _flip(tex_builder_t* tex);
 #define mirror(...) _mirror(&temp, ##__VA_ARGS__)
 void   _mirror(tex_builder_t* tex);
+
+#define TOKEN_PASTE(a, b) a##b
+#define CONCAT(a,b) TOKEN_PASTE(a,b)
+#define UNIQUE_VAR(name) CONCAT(name, __LINE__)
+/* Usage: scoped_begin_end(printf("First"), printf("Last\n")) { ... } */
+#define scoped_begin_end(start, end) \
+    for (int UNIQUE_VAR(_i_) = (start, 0); !UNIQUE_VAR(_i_); (UNIQUE_VAR(_i_) += 1), end)
+
+
+#define scope_tex_build(tex, builder) \
+    for (tex_builder_t temp = builder; temp.i == 0; (temp.i+=1, atlas = _create(temp)))
+#define scope_tex_rect(x,y,h,w) \
+    for (int UNIQUE_VAR(old_x_start) = temp.x_start, UNIQUE_VAR(old_y_start) = temp.y_start, UNIQUE_VAR(old_width) = temp.width, UNIQUE_VAR(old_height) = temp.height, UNIQUE_VAR(j) = (temp = __rect(temp, x + UNIQUE_VAR(old_x_start),y + UNIQUE_VAR(old_y_start),w,h), 0); UNIQUE_VAR(j) == 0; (UNIQUE_VAR(j)+=1, temp.x_start = UNIQUE_VAR(old_x_start), temp.y_start = UNIQUE_VAR(old_y_start), temp.width = UNIQUE_VAR(old_width), temp.height = UNIQUE_VAR(old_height)))
+
+
+
 
 /* usage: tex_build(build(), transform(), transform(), ...) */
 #define tex_build(tex, ...) ({tex_builder_t temp = tex; __VA_ARGS__; _create(temp); })
@@ -109,6 +177,31 @@ void _noise(tex_builder_t* texer, float intensity)  {
         tex->rgb[i].r = fminf(fmaxf(tex->rgb[i].r, 0.0f), 1.0f);
         tex->rgb[i].g = fminf(fmaxf(tex->rgb[i].g, 0.0f), 1.0f);
         tex->rgb[i].b = fminf(fmaxf(tex->rgb[i].b, 0.0f), 1.0f);
+    }
+}
+
+void __noise(tex_builder_t texer, float intensity)  {
+    texture_t* tex = &(texer.tex);
+
+    size_t index = (texer.y_start * texer.atlas_width) + texer.x_start;
+
+    static unsigned int random = 0;
+    srand(time(0) + random);
+    random = rand();
+
+    for (size_t y = texer.y_start; y < (texer.y_start + texer.height); y++) {
+        for (size_t x = texer.x_start; x < (texer.x_start + texer.width); x++) {
+            size_t idx = (y * texer.atlas_width) + x;
+            // Add noise to each color component based on intensity
+            tex->rgb[idx].r += intensity * ((float)rand() / (float) RAND_MAX - 0.5f);
+            tex->rgb[idx].g += intensity * ((float)rand() / (float) RAND_MAX - 0.5f);
+            tex->rgb[idx].b += intensity * ((float)rand() / (float) RAND_MAX - 0.5f);
+
+            // Ensure color components are within [0, 1] range
+            tex->rgb[idx].r = fminf(fmaxf(tex->rgb[idx].r, 0.0f), 1.0f);
+            tex->rgb[idx].g = fminf(fmaxf(tex->rgb[idx].g, 0.0f), 1.0f);
+            tex->rgb[idx].b = fminf(fmaxf(tex->rgb[idx].b, 0.0f), 1.0f);
+        }
     }
 }
 
@@ -222,10 +315,19 @@ void _smear(tex_builder_t* texer, color_t rgb) {
 
 tex_builder_t texture(int w, int h, color_t rgba) {
     tex_builder_t texer;
+
+    texer.atlas_width  = w;
+    texer.atlas_height = h;
+    texer.width        = w;
+    texer.height       = h;
+    texer.x_start      = 0;
+    texer.y_start      = 0;
+    texer.i            = 0;
+
     texer.tex.width        = w;
     texer.tex.height       = h;
-    texer.tex.atlas_width  = w;
-    texer.tex.atlas_height = h;
+    texer.tex.atlas_width  = w; // TODO old
+    texer.tex.atlas_height = h; // TODO old
     texer.tex.x_start      = 0;
     texer.tex.y_start      = 0;
     texer.tex.owns_buffer  = 1;
@@ -237,7 +339,7 @@ tex_builder_t texture(int w, int h, color_t rgba) {
     return texer;
 }
 
-tex_builder_t subtexture(texture_t* atlas, int w, int h, color_t rgba) {
+tex_builder_t _subtexture(texture_t* atlas, int w, int h, color_t rgba) {
     assert(atlas->atlas_width >= w && atlas->atlas_height >= h && "Subtexture is bigger than the atlas\n");
 
     tex_builder_t texer;
